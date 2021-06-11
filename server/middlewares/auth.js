35ff3183c58e45097dd479625e6f8260948a6193
VenansiusRendy <lierendysetiawan3232@gmail.com>
2021-06-11 21:30:57 +0800

    Final Commit

--- server/middlewares/auth.js
+++ server/middlewares/auth.js
@@ -1,1 +1,29 @@
-const auth = (req, res, next) => {};
+const { User } = require("../models");
+const jwt = require("jsonwebtoken");
+const auth = (req, res, next) => {
+	const access_token = req.headers.access_token;
+	if (!access_token) {
+		res.status(404).json("Invalid User / Password");
+	}
+	try {
+		const verify = jwt.verify(access_token, process.env.JWT_SECRET);
+		if (verify) {
+			User.findOne({ where: { email: verify.email } })
+				.then((user) => {
+					if (user) {
+						next();
+					} else if (!user) {
+						res.status(404).json("Invalid User / Password");
+					}
+				})
+				.catch((err) => {
+					console.log(err);
+					res.status(404).json("Invalid User / Password");
+				});
+		} else if (!verify) {
+			res.status(404).json("Invalid User / Password");
+		}
+	} catch (error) {}
+};
+
+module.exports = auth;
--- server/router.js
+++ server/router.js
@@ -1,5 +1,8 @@
 const router = require("express").Router();
-const { User } = require("./models");
+const { User, Inventory } = require("./models");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const auth = require("./middlewares/auth");
 
 router.get("/", (req, res) => {
 	console.log("hello");
@@ -11,6 +14,47 @@ router.post("/register", (req, res) => {
 	User.create({ email, password })
 		.then((user) => {
 			res.status(201).json({ id: user.id, email: user.email });
+		})
+		.catch((err) => {
+			console.log(err);
+			res.status(500).json("Internal Server Error");
+		});
+});
+
+router.post("/login", (req, res) => {
+	User.findOne({
+		where: {
+			email: req.body.email,
+		},
+	})
+		.then((user) => {
+			if (!user) {
+				res.status(404).json("Invalid Password / Email");
+			}
+			const validate = bcrypt.compareSync(
+				String(req.body.password),
+				user.password
+			);
+			if (validate) {
+				const access_token = jwt.sign(
+					{ email: user.email, id: user.id },
+					process.env.JWT_SECRET
+				);
+				res.status(200).json({ access_token });
+			} else if (!validate) {
+				res.status(404).json("Invalid Password / Email");
+			}
+		})
+		.catch((err) => {
+			console.log(err);
+			res.status(500).json("Internal Server Error");
+		});
+});
+
+router.get("/inventories", auth, (req, res) => {
+	Inventory.findAll()
+		.then((inventories) => {
+			res.status(200).json(inventories);
 		})
 		.catch((err) => {
 			console.log(err);
